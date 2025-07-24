e6280896bcf418538c67d12faf3f33c661f5aa71
Nate Douglas <ndouglas@users.noreply.github.com>
2025-07-24 14:06:36 -0400

    Fix some issues with systemwide bash prompt customization.

--- roles/goldentooth.setup_bash_prompt/files/bash_prompt.sh
+++ roles/goldentooth.setup_bash_prompt/files/bash_prompt.sh
@@ -5,8 +5,8 @@ if [[ ! -t 1 ]]; then
   : No prompt needed.
 elif [ ! -x "$(command -v tput)" ]; then
   PS1="\u@\h:\w\$ ";
-elif [ -f "${HOME}/.bash_prompt_local" ]; then
-  PS1="$(source "${HOME}/.bash_prompt_local" 2>/dev/null)";
+elif [ -f "/etc/bash_prompt_local.sh" ]; then
+  PS1="$(source "/etc/bash_prompt_local.sh" 2>/dev/null)";
 else
   PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ ';
 fi
